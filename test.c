797c3f90ddc652894264a83c5bde71f0b40f7cf4
kuonyuma <2006728zcp@gmail.com>
2025-11-01 22:42:29 +0800

    对scanf的输入做出了限制，将运算与进位融合在一个循环里面，lens的大小更加合理

--- test.c
+++ test.c
@@ -4,7 +4,9 @@
 #define MAX_NUM 1000 
 void change(int* nums, char* s, int* plen)
 {
-	scanf("%s", s);
+	char defend[20] = { 0 };
+	snprintf(defend,sizeof(defend), "%%%ds", MAX_NUM - 1);
+	scanf(defend, s);
 	int len = strlen(s);
 	*plen = len;
 	for (int i = 0; i < len; i++)nums[i] = s[len - 1 - i] - '0';
@@ -12,26 +14,24 @@ void change(int* nums, char* s, int* plen)
 void add(int* nums1,int* nums2,int* p1,int* p2,int*sum,int*ps)
 {
 	memset(sum,0,sizeof(int)*2*MAX_NUM);
-
-	int max_num = (*p1 > *p2) ? *p1 : *p2;
-	for (int i = 0;i < max_num; i++)//nums1
-	{
-		sum[i] = nums1[i] + nums2[i];
-	}
-	
+    *ps = (*p1 > *p2) ? *p1 : *p2;
 	int buffer = 0;
-	int k = 0;
-	while (k < *ps)
+	for (int i = 0;i < *ps; i++)
 	{
-		sum[k] += buffer;
-		buffer = sum[k] / 10;
-		sum[k] = sum[k] % 10;
-		k++;
+		int n1 = (i < *p1) ? nums1[i] : 0;
+		int n2 = (i < *p2) ? nums2[i] : 0;
+		sum[i] = n1+n2 + buffer;
+		buffer = sum[i] / 10;
+		sum[i] = sum[i] % 10;
 	}
 	if (buffer != 0)
 	{
-		*ps+=1;
 		sum[*ps] = buffer;
+		(*ps)++;
+	}
+	if (*ps == 0)
+	{
+		*ps = 1;
 	}
 }
 void print(int* sum,int* lens)
@@ -48,7 +48,6 @@ void print(int* sum,int* lens)
 }
 int main()
 {
-	
 	char s1[MAX_NUM];
 	char s2[MAX_NUM];
 	int num_1[MAX_NUM] = { 0 };
@@ -56,10 +55,9 @@ int main()
 	int len1, len2;
 	change(num_1,s1,&len1);
 	change(num_2,s2,&len2);
-	int sum[2 * MAX_NUM];
-	int lens = len1 + len2;
+	int sum[2*MAX_NUM];
+	int lens = 0;
 	add(num_1,num_2,&len1,&len2,sum,&lens);
 	print(sum,&lens);
-	
 	return 0;
 }
